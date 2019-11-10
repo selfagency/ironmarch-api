0e3e7116d5a53b2e5237afba9f27c9c3b94dcf9e
The Jewish Worker <hello@jewishworker.org>
2019-11-10 00:56:12 -0500

    add doxed query

--- src/methods/user.js
+++ src/methods/user.js
@@ -3,7 +3,7 @@ const { User } = require('../models')
 
 const user = async params => {
   try {
-    let { id, limit, offset, sort, order, terms } = params,
+    let { id, limit, offset, sort, order, terms, dox } = params,
       output,
       include = [],
       where = {}
@@ -13,9 +13,11 @@ const user = async params => {
     offset = offset ? parseInt(offset) : null
     sort = sort ? sort : 'id'
     order = order ? [[sort, order]] : [[sort, 'DESC']]
+    dox = dox === 'true' ? true : null
 
     include.push({ association: 'statuses', order: [['date', 'DESC']] })
     if (terms) where.name = { [Op.like]: `%${terms}%` }
+    if (dox) where.lookup = { [Op.ne]: null }
 
     if (id) {
       output = await User.findOne({ where: { id: { [Op.eq]: id } }, order, include })
