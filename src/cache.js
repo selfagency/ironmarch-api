f8352abe133eb99df78925066f8a0c7da4e85dce
The Jewish Worker <hello@jewishworker.org>
2019-11-10 05:37:26 -0500

    long-term caching

--- src/cache.js
+++ src/cache.js
@@ -3,7 +3,7 @@ const cache = require('express-redis-cache')({
   port: process.env.CACHE_PORT,
   auth_pass: process.env.CACHE_PASS,
   prefix: 'iron-march',
-  expire: { '4xx': 1, '5xx': 1, xxx: process.env.ENVIRONMENT === 'development' ? 60 : 3600 }
+  expire: { '4xx': 1, '5xx': 1, xxx: process.env.ENVIRONMENT === 'development' ? 60 : 43200 }
 })
 
 module.exports = cache
